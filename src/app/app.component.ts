7da34b28c7df573780d5073a9f7c47773af7dcd6
kuldeep <kuldeepvatsal@gmail.com>
2021-07-14 10:05:20 +0530

    committ changeslesson3

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit 
+{
+  title = 'The Datign App';
+  users:any;
+
+  constructor(private http:HttpClient){}
+   ngOnInit()  
+    {
+       this.getUsers();
+     }
+     getUsers()
+     {
+      this.http.get('https://localhost:44382/api/users').
+      subscribe(response=>{this.users=response},error=>{console.log(error)});
+  
+     }
+}
